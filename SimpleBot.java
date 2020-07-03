1c8f1bfd4018b8e7162e2e1ba2a130ae0f53e2c1
Tolokonov Ilya <57015678+tolokonov@users.noreply.github.com>
2020-07-03 20:08:22 +0300

    Update SimpleBot.java

--- SimpleBot.java
+++ SimpleBot.java
@@ -3,10 +3,10 @@ package bot;
 import java.util.Scanner;
 
 public class SimpleBot {
-    final static Scanner scanner = new Scanner(System.in); // Do not change this line
+    final static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        greet("Gideon", "2020"); // change it as you need
+        greet("Gideon", "2020");
         remindName();
         guessAge();
         count();
@@ -56,6 +56,6 @@ public class SimpleBot {
     }
 
     static void end() {
-        System.out.println("Congratulations, have a nice day!"); // Do not change this text
+        System.out.println("Congratulations, have a nice day!");
     }
 }
